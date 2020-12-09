18b5b98beae0533bc2b2ba363a36236dbd3327b1
Scott Anderson <scotta@skyscanner.net>
2020-12-09 19:58:11 +0000

    WIP - day 3

---
+++ 2020-12-03/src/solution.js
@@ -0,0 +1,43 @@
+class Policy {
+  constructor(policyString) {
+    const [ occurrenceKey, character ] = policyString.split(' ');
+    const [ minOccurence, maxOccurrence ] = occurrenceKey.split('-').map((i) => parseInt(i, 10));
+    this.minOccurence = minOccurence;
+    this.maxOccurrence = maxOccurrence;
+    this.character = character;
+  }
+
+  isMetByPart1(password) {
+    const matches = (password.match(new RegExp(this.character, 'g')) || []).length;
+    return this.minOccurence <= matches && this.maxOccurrence >= matches;
+  }
+
+  isMetByPart2(password) {
+    const firstIndex = this.minOccurence-1;
+    const secondIndex = this.maxOccurrence-1;
+    const firstChar = password.charAt(firstIndex);
+    const secondChar = password.charAt(secondIndex);
+    const matches = [firstChar.match(new RegExp(this.character)) !== null, secondChar.match(new RegExp(this.character)) !== null];
+    const numMatches = matches.filter(x => !!x).length;
+    return numMatches === 1;
+  }
+}
+
+const splitData = (row) => {
+  return row.split('');
+}
+
+const part1 = (input) => {
+  console.log(input)
+  const data = input.map(x => splitData(x))
+  const matches = data.map(([policy, password]) => policy.isMetByPart1(password));
+  return matches.filter(x => !!x).length;
+};
+
+const part2 = (input) => {
+  const data = input.map(x => splitData(x))
+  const matches = data.map(([policy, password]) => policy.isMetByPart2(password));
+  return matches.filter(x => !!x).length;
+};
+
+module.exports = { part1, part2 };
